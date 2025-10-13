1cfeaad26e9ce25b8137d1e1f0d86b5e1ad41579
Hüsam <husamahmud@gmail.com>
2025-10-14 02:36:20 +0300

    fix build

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig, fontProviders } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
