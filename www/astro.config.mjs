d71418219eb7065264e1d433b22b15b93b766f08
Hüsam <husamahmud@gmail.com>
2025-10-14 00:54:59 +0300

    website

---
+++ www/astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Roboto Mono",
+      cssVariable: "--font-roboto-mono"
+    }]
+  },
+
+  integrations: [react()],
+  adapter: cloudflare()
+});
