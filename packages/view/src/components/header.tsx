4ea4b89cc25798c7be05841d504e6361b8b072e5
Hüsam <husamahmud@gmail.com>
2025-10-14 16:47:31 +0300

    view header style

--- packages/view/src/components/header.tsx
+++ packages/view/src/components/header.tsx
@@ -1,26 +1,36 @@
 import { IoLogoGithub } from "react-icons/io";
-import { cn } from "@/utils/cn";
-import { WEBSITE_URL } from "@/utils/constants";
+import { Button } from "@/components/ui/button";
+import { REPO_URL, WEBSITE_URL } from "@/utils/constants";
 
 export const Header = () => {
 	return (
-		<header className={cn("pb-3 w-full flex items-center justify-between")}>
+		<header className="pb-4 w-full flex items-center justify-between">
 			<a
 				href={WEBSITE_URL}
 				className="border rounded-md group transition-all duration-300 hover:shadow-xl hover:shadow-neutral-800/50"
 			>
 				<img
 					src="/icon.svg"
-					alt="Viz"
+					alt="Hviz logo"
 					className="size-8 transition-transform duration-300 group-hover:scale-105"
 					width={28}
 					height={28}
 				/>
 			</a>
 
-			<a href="https://github.com/husamql3/viz" target="_blank" rel="noopener noreferrer" className="group">
-				<IoLogoGithub className="size-5 text-neutral-400 duration-150 group-hover:text-neutral-500 dark:group-hover:text-neutral-300 dark:text-neutral-600" />
-			</a>
+			<div className="flex items-center gap-2">
+				<a href={WEBSITE_URL} target="_blank" rel="noopener noreferrer" className="group">
+					<Button variant="ghost" size="sm" className="dark:group-hover:text-neutral-300 text-zinc-400">
+						Roadmap
+					</Button>
+				</a>
+
+				<a href={REPO_URL} target="_blank" rel="noopener noreferrer" className="group">
+					<Button variant="ghost" size="sm" className="dark:group-hover:text-neutral-300 text-zinc-400">
+						<IoLogoGithub />
+					</Button>
+				</a>
+			</div>
 		</header>
 	);
 };
--- packages/view/src/utils/constants.ts
+++ packages/view/src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const WEBSITE_URL = "https://www.hviz.com/";
+export const REPO_URL = "https://github.com/husamql3/hviz";
