ec90d99c20d5742c9d19007b6ea0e27905919e4c
Hüsam <husamahmud@gmail.com>
2025-10-16 22:12:55 +0300

    icons sizes

--- www/app/components/header.tsx
+++ www/app/components/header.tsx
@@ -10,12 +10,16 @@ export const Header = () => {
 				alt="hviz logo"
 				width={32}
 				height={32}
-				className="border rounded-md group size-8 duration-300 group-hover:scale-105 transition-all hover:shadow-xl hover:shadow-neutral-800/50"
+				className="border rounded-md group size-9 duration-300 group-hover:scale-105 transition-all hover:shadow-xl hover:shadow-neutral-800/50"
 			/>
 
 			<a href={REPO_URL} target="_blank" rel="noopener noreferrer" className="group">
-				<Button variant="ghost" className="dark:group-hover:text-neutral-300 size-8 text-zinc-400">
-					<IoLogoGithub />
+				<Button
+					variant="ghost"
+					className="dark:group-hover:text-neutral-300 text-zinc-400"
+					size="icon-lg"
+				>
+					<IoLogoGithub className="size-5" />
 				</Button>
 			</a>
 		</header>
